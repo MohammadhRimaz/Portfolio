eb57824e06784f53db79ffc72c1760704cbe6531
MohammadhRimaz <mohammadhrimaz@gmail.com>
2024-06-05 15:13:10 +0530

    Enhanced the Designs of header section

--- scripts.js
+++ scripts.js
@@ -22,7 +22,11 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 });
 
 // Call the function to create shapes with reduced animation speed
-createShapes();
+document.addEventListener('DOMContentLoaded', function(){
+    createShapes();
+    setupNavigation();
+});
+
 
 function createShapes() {
     const backgroundAnimation = document.getElementById('background-animation');
@@ -37,4 +41,16 @@ function createShapes() {
         shape.style.animationDuration = `${20 + Math.random() * 20}s`; // Adjust animation speed
         backgroundAnimation.appendChild(shape);
     }
+}
+
+function setupNavigation() {
+    const link = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
 }
